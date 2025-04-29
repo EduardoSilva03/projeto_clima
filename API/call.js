b77aaaad7f052d76b1bda5dae83022f55405ca8c
EduardoSilva03 <eduaardo.silva03@gmail.com>
2025-04-28 22:30:34 -0300

    call api

---
+++ API/call.js
@@ -0,0 +1,22 @@
+/*
+www.weatherapi.com
+API key = 95134c8ca7c342ab9bb00912252904
+Base url: http://api.weatherapi.com/v1
+*/
+
+function buscar() {
+    const cidade = document.getElementById("cidade").value;
+
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=95134c8ca7c342ab9bb00912252904&q=${cidade}`)
+        .then(response => {
+            console.log(response.data);
+            const nomeCidade = response.data.location.name;
+            const tbody = document.querySelector("tbody");
+            tbody.innerHTML = `<tr><td>${nomeCidade}</td></tr>`;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    console.log(buscar);
+}
