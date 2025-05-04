583efa4d37c8fda840f858c111eaeec7acf14c00
EduardoSilva03 <eduaardo.silva03@gmail.com>
2025-05-04 16:42:24 -0300

    Adicionado mais campos na consulta

--- API/call.js
+++ API/call.js
@@ -14,8 +14,15 @@ function buscar() {
             const estadoCidade = response.data.location.region;
             const paisCidade = response.data.location.country;
             const localtimeCidade = response.data.location.localtime;
+            const periodoCidade = response.data.current.is_day === 1 ? "Dia" : "Noite";
+            const climaCidade = response.data.current.condition.text;
+            const temperaturaCidade = response.data.current.temp_c + " °C";
+            const ventoCidade = response.data.current.wind_kph + " km/h";
+            const umidadecidade = response.data.current.humidity + "%";
             const tbody = document.querySelector("tbody");
-            tbody.innerHTML = `<tr><td>${nomeCidade}</td><td>${estadoCidade}</td><td>${paisCidade}</td><td>${localtimeCidade}</td></tr>`;
+            tbody.innerHTML = `<tr><td>${nomeCidade}</td><td>${estadoCidade}</td><td>${paisCidade}</td>
+                               <td>${localtimeCidade}</td><td>${periodoCidade}</td><td>${climaCidade}</td>
+                               <td>${temperaturaCidade}</td><td>${ventoCidade}</td><td>${umidadecidade}</td></tr>`;
         })
         .catch(error => {
             console.error(error);
